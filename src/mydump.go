534a3efcf419a6f81e84a4fad144f7d075dbe584
jasonshi5310 <jasonshi5310@gmail.com>
2021-03-11 17:00:56 -0800

    report finished

--- src/mydump.go
+++ src/mydump.go
@@ -1,6 +1,7 @@
 // Minqi Shi
 // 111548035
 // References:
+// And there are other references in the code
 // 	https://www.tcpdump.org/pcap.html
 // 	https://www.tcpdump.org/index.html#documentation
 // 	https://pkg.go.dev/github.com/google/gopacket/
@@ -13,42 +14,59 @@
 
 package main
 
-
 import (
 	"fmt"
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/pcap"
-	"github.com/google/gopacket/layers"
 	"os"
 	"strings"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
-
-func tcp_flags(tcp *layers.TCP) string{
+func tcp_flags(tcp *layers.TCP) string {
 	s := ""
 	// FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
-	if tcp.FIN {s+= " FIN"}
-	if tcp.SYN {s+= " SYN"}
-	if tcp.RST {s+= " RST"}
-	if tcp.PSH {s+= " PSH"}
-	if tcp.ACK {s+= " ACK"}
-	if tcp.URG {s+= " URG"}
-	if tcp.ECE {s+= " ECE"}
-	if tcp.CWR {s+= " CWR"}
-	if tcp.NS {s+= " NS"}
+	if tcp.FIN {
+		s += " FIN"
+	}
+	if tcp.SYN {
+		s += " SYN"
+	}
+	if tcp.RST {
+		s += " RST"
+	}
+	if tcp.PSH {
+		s += " PSH"
+	}
+	if tcp.ACK {
+		s += " ACK"
+	}
+	if tcp.URG {
+		s += " URG"
+	}
+	if tcp.ECE {
+		s += " ECE"
+	}
+	if tcp.CWR {
+		s += " CWR"
+	}
+	if tcp.NS {
+		s += " NS"
+	}
 	return s
 }
 
-func ethernet_type_convert(et layers.EthernetType) string{
+func ethernet_type_convert(et layers.EthernetType) string {
 	// https://pkg.go.dev/github.com/notti/gopacket/layers#EthernetType
 	const (
 		// EthernetTypeLLC is not an actual ethernet type.  It is instead a
 		// placeholder we use in Ethernet frames that use the 802.3 standard of
 		// srcmac|dstmac|length|LLC instead of srcmac|dstmac|ethertype.
-		EthernetTypeLLC                         layers.EthernetType = 0
-		EthernetTypeIPv4                        layers.EthernetType = 0x0800
-		EthernetTypeARP                         layers.EthernetType = 0x0806
-		EthernetTypeIPv6                        layers.EthernetType = 0x86DD
+		EthernetTypeLLC  layers.EthernetType = 0
+		EthernetTypeIPv4 layers.EthernetType = 0x0800
+		EthernetTypeARP  layers.EthernetType = 0x0806
+		EthernetTypeIPv6 layers.EthernetType = 0x86DD
 		// EthernetTypeCiscoDiscovery              EthernetType = 0x2000
 		// EthernetTypeNortelDiscovery             EthernetType = 0x01a2
 		// EthernetTypeTransparentEthernetBridging EthernetType = 0x6558
@@ -63,66 +81,60 @@ func ethernet_type_convert(et layers.EthernetType) string{
 		// EthernetTypeLinkLayerDiscovery          EthernetType = 0x88cc
 		// EthernetTypeEthernetCTP                 EthernetType = 0x9000
 	)
-	if et == EthernetTypeLLC{
+	if et == EthernetTypeLLC {
 		return "0x0000"
-	}else if et == EthernetTypeIPv4{
+	} else if et == EthernetTypeIPv4 {
 		return "0x0800"
-	}else if et == EthernetTypeIPv6{
+	} else if et == EthernetTypeIPv6 {
 		return "0x86DD"
-	}else if et == EthernetTypeARP{
+	} else if et == EthernetTypeARP {
 		return "0x0806"
-	}else {
+	} else {
 		return "not found"
 	}
 }
 
 // The following function is inspired from the following link
-// https://www.itread01.com/content/1546724723.html
+// https://www.devdungeon.com/content/packet-capture-injection-and-analysis-gopacket
 func printPacketInfo(packet gopacket.Packet, dash_s string) {
 	applicationLayer := packet.ApplicationLayer()
-    if dash_s != "-1"{
-		if applicationLayer == nil{
+	if dash_s != "-1" {
+		if applicationLayer == nil {
 			return
 		}
-        // Search for a string inside the payload
-        if !strings.Contains(string(applicationLayer.Payload()), dash_s) {
-            return
-        }
-    }
-	// Time
-	fmt.Printf("%v",packet.Metadata().Timestamp.Format("2006-01-02 15:04:05.000000 "))
-
-
-    // Let's see if the packet is an ethernet packet
-    ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
-    if ethernetLayer != nil {
-        ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
-        fmt.Print(ethernetPacket.SrcMAC, " -> ")
-        fmt.Print(ethernetPacket.DstMAC, " ")
-        // Ethernet type is typically IPv4 but could be ARP or other
-		hex_type := ethernet_type_convert(ethernetPacket.EthernetType)
-		if hex_type == "not found"{
-			fmt.Print("type ", ethernetPacket.EthernetType)
-		}else{
-        	fmt.Print("type ", hex_type)
+		// Search for a string inside the payload
+		if !strings.Contains(string(applicationLayer.Payload()), dash_s) {
+			return
 		}
-    }
+	}
+	// Time
+	fmt.Printf("%v", packet.Metadata().Timestamp.Format("2006-01-02 15:04:05.000000 "))
+
+	// ethernet
+	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethernetLayer != nil {
+		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+		fmt.Print(ethernetPacket.SrcMAC, " -> ")
+		fmt.Print(ethernetPacket.DstMAC, " ")
+		// Ethernet type is typically IPv4 but could be ARP or other
+		hex_type := ethernet_type_convert(ethernetPacket.EthernetType)
+		if hex_type == "not found" {
+			fmt.Print("type ", ethernetPacket.EthernetType)
+		} else {
+			fmt.Print("type ", hex_type)
+		}
+	}
+
+	// packet length
 	packet_length := packet.Metadata().CaptureInfo.Length
 	fmt.Println(" len", packet_length)
-	
-    // Let's see if the packet is IP (even though the ether type told us)
-    ipLayer := packet.Layer(layers.LayerTypeIPv4)
-    if ipLayer != nil {
-        // fmt.Println("IPv4 layer detected.")
-        ip, _ := ipLayer.(*layers.IPv4)
 
-        // IP layer variables:
-        // Version (Either 4 or 6)
-        // IHL (IP Header Length in 32-bit words)
-        // TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
-        // Checksum, SrcIP, DstIP
+	// IP
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		ip, _ := ipLayer.(*layers.IPv4)
 
-		// Let's see if the packet is TCP
+		// TCP or UDP
 		tcpLayer := packet.Layer(layers.LayerTypeTCP)
 		udplayer := packet.Layer(layers.LayerTypeUDP)
 		if tcpLayer != nil {
@@ -130,38 +142,38 @@ func printPacketInfo(packet gopacket.Packet, dash_s string) {
 			fmt.Printf("%s.%s -> %s.%s", ip.SrcIP, tcp.SrcPort, ip.DstIP, tcp.DstPort)
 			fmt.Print(" ", ip.Protocol)
 			fmt.Print(tcp_flags(tcp))
-		}else if udplayer != nil {
+		} else if udplayer != nil {
 			udp, _ := udplayer.(*layers.UDP)
 			fmt.Printf("%s.%s -> %s.%s", ip.SrcIP, udp.SrcPort, ip.DstIP, udp.DstPort)
 			fmt.Print(" ", ip.Protocol)
-		}else{
+		} else {
 			fmt.Printf("%s -> %s", ip.SrcIP, ip.DstIP)
 			fmt.Print(" ", ip.Protocol)
 		}
 		fmt.Println()
-    }
+	}
 
 	// print the payload
 	if applicationLayer != nil {
 		pl := gopacket.LayerDump(applicationLayer)
-		pl = pl[strings.Index(pl,"\n")+1:]
+		pl = pl[strings.Index(pl, "\n")+1:]
 		fmt.Print(pl)
-    }
-	
-    // Check for errors
-    if err := packet.ErrorLayer(); err != nil {
-        fmt.Println("Error decoding some part of the packet:", err)
-    }
+	}
+
+	// Check for errors
+	if err := packet.ErrorLayer(); err != nil {
+		fmt.Println("Error decoding some part of the packet:", err)
+	}
 	fmt.Println()
 	// fmt.Println("----------------------------------------------")
 }
 
 func main() {
 	defer func() {
-        if r := recover(); r != nil {
-            fmt.Println("", r)
-        }
-    }()
+		if r := recover(); r != nil {
+			fmt.Println("", r)
+		}
+	}()
 	// fmt.Println("Hello world!")
 	argv := os.Args[1:]      // Argument vector
 	argv_length := len(argv) // Length of the arguments
@@ -177,11 +189,6 @@ func main() {
 		expr_string string
 		optind      int = 0
 	)
-
-	// if argv_length == 0 {
-	// 	fmt.Println("Missing arguments!")
-	// 	return
-	// }
 
 	for i := 0; i < argv_length; i = i + 2 {
 		var opt string = argv[i]
@@ -240,11 +247,11 @@ func main() {
 			}
 		}
 		// fmt.Printf("Expr String: %v\n", expr_string)
-	}else {
+	} else {
 		expr_string = ""
 	}
 	//https://www.devdungeon.com/content/packet-capture-injection-and-analysis-gopacket
-	if inter_face == "-1"{
+	if inter_face == "-1" {
 		devices, err := pcap.FindAllDevs()
 		if err != nil {
 			panic(err)
@@ -256,31 +263,28 @@ func main() {
 	if filepath != "-1" {
 		if handle, err := pcap.OpenOffline(filepath); err != nil {
 			panic(err)
-		} else if err := handle.SetBPFFilter(expr_string); err != nil {  // BPF
-			panic(err)		
+		} else if err := handle.SetBPFFilter(expr_string); err != nil { // BPF
+			panic(err)
 		} else {
 			defer handle.Close()
 			packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 			for packet := range packetSource.Packets() {
-				// handlePacket(packet) // Do something with a packet here.
-				// fmt.Printf("%T",packet)
 				printPacketInfo(packet, str)
 			}
 		}
-	// live cap
+		// live cap
 	} else {
 		if handle, err := pcap.OpenLive(inter_face, 1600, true, pcap.BlockForever); err != nil {
 			panic(err)
-		  } else if err := handle.SetBPFFilter(expr_string); err != nil {  // BPF
+		} else if err := handle.SetBPFFilter(expr_string); err != nil { // BPF
 			panic(err)
-		  } else {
+		} else {
 			defer handle.Close()
 			packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 			for packet := range packetSource.Packets() {
 				printPacketInfo(packet, str)
 			}
-		  }
+		}
 	}
-
 
 }
